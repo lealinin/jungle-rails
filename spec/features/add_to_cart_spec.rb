433f5b43daf37ed67347aea440422044badbdb72
Lea Linin <lea.linin@gmail.com>
2020-02-12 04:57:47 +0000

    add add_to_cart feature spec

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+
+   before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+    
+  end
+  
+  scenario "They see number inside My Cart increase by 1" do
+  
+    visit root_path
+
+    within('#navbar') { expect(page).to have_content('My Cart (0)') }
+
+    find('.btn-primary', match: :prefer_exact).click
+
+    within('#navbar') { expect(page).to have_content('My Cart (1)') }
+
+    save_screenshot
+
+  end
+end
